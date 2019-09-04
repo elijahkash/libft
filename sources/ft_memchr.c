eba7f025d98f89b1c2d21ead3556c4ed77feeb12
Midas Trisha <mtrisha@ox-b2.21-school.ru>
2019-09-04 23:21:29 +0300

    fix issue

--- sources/ft_memchr.c
+++ sources/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: mtrisha <mtrisha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 22:50:48 by mtrisha           #+#    #+#             */
-/*   Updated: 2019/09/04 23:15:30 by mtrisha          ###   ########.fr       */
+/*   Updated: 2019/09/04 23:21:10 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ void	*ft_memchr(const void *s, int c, size_t n)
 
 	ptr = (unsigned char *)s;
 	while (n-- > 0)
+	{
 		if (*ptr == (unsigned char)c)
 			return ((void *)ptr);
+		ptr++;
+	}
 	return (NULL);
 }
