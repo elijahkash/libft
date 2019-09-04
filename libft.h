a369ed2d232d46becd8c36db78a368348d7dcc54
Midas Trisha <mtrisha@oa-b4.21-school.ru>
2019-09-04 16:57:47 +0300

    in work...

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mtrisha <mtrisha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 22:15:30 by mtrisha           #+#    #+#             */
-/*   Updated: 2019/09/04 16:23:17 by mtrisha          ###   ########.fr       */
+/*   Updated: 2019/09/04 16:48:30 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@
 
 size_t	ft_strlen(const char *str);
 int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
