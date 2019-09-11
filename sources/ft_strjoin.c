8193f9e615db75cc979cf4c2fdb6874aa39bbbc6
Midas Trisha <mtrisha@ox-c3.21-school.ru>
2019-09-11 11:43:37 +0300

    fix oversize

--- sources/ft_strjoin.c
+++ sources/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mtrisha <mtrisha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 14:49:48 by mtrisha           #+#    #+#             */
-/*   Updated: 2019/09/06 20:09:05 by mtrisha          ###   ########.fr       */
+/*   Updated: 2019/09/11 11:42:27 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char *str;
+	char	*str;
+	size_t	len1;
+	size_t	len2;
 
-	str = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (len1 + len2 > __SIZE_MAX__ - 1)
+		return (NULL);
+	str = ft_strnew(len1 + len2);
 	if (!str)
 		return (NULL);
 	ft_memcpy((void *)str, s1, ft_strlen(s1));
