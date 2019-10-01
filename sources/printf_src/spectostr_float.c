15aa47581e7fe1bb021c528d59ef8f834db5b7cb
Midas Trisha <mtrisha@ox-b5.21-school.ru>
2019-10-01 18:10:05 +0300

    chenge arr size in strtofloat

--- sources/printf_src/spectostr_float.c
+++ sources/printf_src/spectostr_float.c
@@ -6,7 +6,7 @@
 /*   By: mtrisha <mtrisha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 15:47:30 by mtrisha           #+#    #+#             */
-/*   Updated: 2019/09/30 20:54:43 by mtrisha          ###   ########.fr       */
+/*   Updated: 2019/10/01 18:09:37 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@
 char	*spectostr_float(t_specifications_def spec, va_list argptr)
 {
 	char		*output;
-	char		tmp[8000];
+	char		tmp[20000];
 	int			len;
 	int			extralen;
 	long double	test;
 
 	test = get_float_item_by_size(argptr, spec.sizes);
 	tmp[0] = '\0';
-	prf_dblcalc(test, (spec.precision > 5000) ? 5000 : spec.precision, tmp);
+	prf_dblcalc(test, (spec.precision > 19000) ? 19000 : spec.precision, tmp);
 	len = ft_strlen(tmp);
 	if (spec.flags & FLAG_OCTT && !ft_strchr(tmp, '.')
 		&& !ft_strstr(tmp, "inf") && !ft_strstr(tmp, "nan"))
