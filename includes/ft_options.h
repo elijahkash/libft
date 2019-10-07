0e62cc559d3197a79ae1f8b3bd23c591499702d2
Midas Trisha <mtrisha@oa-b2.21-school.ru>
2019-10-07 20:30:00 +0300

    fix options.h

--- includes/ft_options.h
+++ includes/ft_options.h
@@ -6,7 +6,7 @@
 /*   By: mtrisha <mtrisha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/02 13:58:10 by mtrisha           #+#    #+#             */
-/*   Updated: 2019/10/07 18:44:50 by mtrisha          ###   ########.fr       */
+/*   Updated: 2019/10/07 20:29:49 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ typedef unsigned long long int	t_options;
 
 int								ft_is_option(const char *line);
 char							ft_check_opt(char *line, char *base);
-int								ft_is_validopt(char *line);
 t_options						ft_get_options(const char *line);
+t_options						ft_get_opt_bit(const char c);
 
 #endif
