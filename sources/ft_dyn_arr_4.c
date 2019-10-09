0708d7ae44cc996799e04433cce71ca21f47bace
Midas Trisha <mtrisha@su-h6.21-school.ru>
2019-10-09 17:17:02 +0300

    fix darr_add_str

--- sources/ft_dyn_arr_4.c
+++ sources/ft_dyn_arr_4.c
@@ -6,7 +6,7 @@
 /*   By: mtrisha <mtrisha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 15:33:55 by mtrisha           #+#    #+#             */
-/*   Updated: 2019/10/09 15:40:14 by mtrisha          ###   ########.fr       */
+/*   Updated: 2019/10/09 17:16:54 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void		*darr_add_str(t_darr arr, void *data)
 
 	if (*arr.curlen == *arr.max_len)
 		darr_extend(arr);
-	newstr = ft_malloc(ft_strlen(data));
+	newstr = ft_malloc(ft_strlen(data) + 1);
 	ft_strcpy(newstr, data);
 	ft_memcpy(darr(arr, *arr.curlen), &newstr, *arr.item_size);
 	(*arr.curlen)++;
