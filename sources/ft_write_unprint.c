9e9b2b0effd04d098a812ab6aacb7d1864beeb2f
Midas Trisha <mtrisha@il-i6.21-school.ru>
2019-10-20 16:10:51 +0300

    fix write_unprint

--- sources/ft_write_unprint.c
+++ sources/ft_write_unprint.c
@@ -6,7 +6,7 @@
 /*   By: mtrisha <mtrisha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 14:36:33 by mtrisha           #+#    #+#             */
-/*   Updated: 2019/10/20 14:42:39 by mtrisha          ###   ########.fr       */
+/*   Updated: 2019/10/20 16:10:40 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,17 @@
 int		ft_write_unprint(int fd, char *str, size_t len)
 {
 	size_t ret;
+	size_t i;
 
 	ret = 0;
+	i = 0;
 	while (len--)
 	{
-		if (ft_isprint(str[len]))
-			ret += write(fd, str + len, 1);
+		if (ft_isprint(str[i]))
+			ret += write(fd, str + i, 1);
 		else
 			ret += write(fd, "\e[41m \e[0m", 10);
+		i++;
 	}
 	return (ret);
 }
