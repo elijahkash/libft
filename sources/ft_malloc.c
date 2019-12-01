4e0f5ed960f2ec88925cae4614ffaa6def1fceb2
Midas Trisha <mtrisha@student.21-school.ru>
2019-12-01 16:47:06 +0300

    fix ft_malloc

--- sources/ft_malloc.c
+++ sources/ft_malloc.c
@@ -6,13 +6,13 @@
 /*   By: mtrisha <mtrisha@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 16:30:21 by mtrisha           #+#    #+#             */
-/*   Updated: 2019/11/26 19:30:25 by mtrisha          ###   ########.fr       */
+/*   Updated: 2019/12/01 16:46:30 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_memory_manager.h>
 
-#include <ft_libft_mod.h>
+#include <libft.h>
 
 #include <stdlib.h>
 
@@ -31,7 +31,7 @@ void			*ft_malloc(size_t size)
 {
 	register void	*tmp;
 
-	if (!(tmp = malloc(size))
+	if (!(tmp = malloc(size)))
 		ft_error_exit(ERR_MEMALLOC_MSG, 0);
 	return (tmp);
 }
--- sources/ft_remalloc.c
+++ sources/ft_remalloc.c
@@ -6,14 +6,13 @@
 /*   By: mtrisha <mtrisha@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/10 17:43:04 by mtrisha           #+#    #+#             */
-/*   Updated: 2019/12/01 16:42:15 by mtrisha          ###   ########.fr       */
+/*   Updated: 2019/12/01 16:46:28 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_memory_manager.h>
 
-#include <ft_libc.h>
-#include <ft_libft_mod.h>
+#include <libft.h>
 
 #include <stdlib.h>
 
