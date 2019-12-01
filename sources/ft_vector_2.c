7382546d5034b8c6ccc1875f0c6b76ee8c9b2dff
Midas Trisha <mtrisha@student.21-school.ru>
2019-12-01 16:31:17 +0300

    some optimise in  vect_

--- sources/ft_vector_2.c
+++ sources/ft_vector_2.c
@@ -6,7 +6,7 @@
 /*   By: mtrisha <mtrisha@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/04 20:48:37 by mtrisha           #+#    #+#             */
-/*   Updated: 2019/11/29 14:13:40 by mtrisha          ###   ########.fr       */
+/*   Updated: 2019/12/01 16:30:24 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,6 @@ inline void		*vect_top(t_vect *restrict arr)
 
 void			*vect_(t_vect *restrict arr, long long int i)
 {
-	if (UNLIKELY(i == 0))
-		return (vect_top(arr));
-	while (UNLIKELY((size_t)ABS_LL(i) >= arr->curlen))
-		i += (i > 0) ? arr->curlen * -1 : arr->curlen;
 	return ((i >= 0) ? vect(arr, arr->curlen - 1 - i) :
 						vect(arr, (i * -1) - 1));
 }
