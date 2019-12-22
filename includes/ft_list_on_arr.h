73079caa7faaa3f3a7bfa092e9c2b235c10bb458
Midas Trisha <mtrisha@student.21-school.ru>
2019-12-22 21:52:37 +0300

    start working under alst

---
+++ includes/ft_list_on_arr.h
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_on_arr.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtrisha <mtrisha@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/22 18:31:03 by mtrisha           #+#    #+#             */
+/*   Updated: 2019/12/22 21:45:39 by mtrisha          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_ON_ARR_H
+# define FT_LIST_ON_ARR_H
+
+# include <string.h>
+
+//TODO: restrict inline
+
+typedef struct	s_alst_item
+{
+	size_t	self;
+	size_t	next;
+}				t_alst_item;
+
+typedef struct	s_list_on_arr
+{
+	void		*mem;
+	__uint64_t	*free_space_mask;
+	void		*items;
+	t_alst_item	*list;
+	size_t		head;
+	size_t		tail;
+	size_t		curlen;
+	size_t		item_size;
+	size_t		max_size;
+}				t_alst;
+
+# define ALST_SPEC_VALUE ((size_t)(0 - 1))
+
+void			alst_init(t_alst *self, size_t item_size,
+										size_t init_len);
+void			alst_del(t_alst *self);
+void			alst_extend(t_alst *self);
+
+size_t			alst_get_space(t_alst *self);
+
+t_alst_item		*alst_add_head(t_alst *self, void *data);
+
+#endif
---
+++ sources/ft_list_on_arr/ft_list_on_arr_1.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_on_arr_1.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtrisha <mtrisha@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/22 18:32:14 by mtrisha           #+#    #+#             */
+/*   Updated: 2019/12/22 21:52:12 by mtrisha          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_list_on_arr.h>
+
+#include <libft.h>
+
+void			alst_init(t_alst *self, size_t item_size, size_t init_len)
+{
+	size_t	free_mem_mask_size;
+
+	self->item_size = item_size;
+	self->curlen = 0;
+	self->max_size = init_len;
+	self->head = ALST_SPEC_VALUE;
+	self->tail = ALST_SPEC_VALUE;
+	free_mem_mask_size = (init_len / 64 + 1) * sizeof(__uint64_t);
+	self->mem = ft_malloc((item_size + sizeof(t_alst_item)) * init_len +
+							free_mem_mask_size);
+	self->free_space_mask = self->mem;
+	self->items = self->mem + free_mem_mask_size;
+	self->list = self->mem + free_mem_mask_size + init_len * item_size;
+	ft_memset(self->free_space_mask, -1, free_mem_mask_size);
+}
+
+void			alst_del(t_alst *self)
+{
+	if (self->mem)
+		ft_free(self->mem);
+	ft_bzero(self, sizeof(t_alst));
+}
+
+void			alst_extend(t_alst *self)
+{
+	void	*tmp;
+	size_t	old_size;
+	size_t	new_free_mem_mask_size;
+	size_t	old_free_mem_mask_size;
+
+	old_size = self->max_size;
+	self->max_size <<= 1;
+	new_free_mem_mask_size = (self->max_size / 64 + 1) * sizeof(__uint64_t);
+	old_free_mem_mask_size = (old_size / 64 + 1) * sizeof(__uint64_t);
+	tmp = ft_malloc((self->item_size + sizeof(t_alst_item)) * self->max_size +
+						new_free_mem_mask_size);
+	ft_memset(self->free_space_mask, -1, new_free_mem_mask_size);
+	self->free_space_mask = ft_memcpy(tmp, self->mem, old_free_mem_mask_size);
+	self->items = ft_memcpy(tmp + new_free_mem_mask_size, self->items,
+							old_size * self->item_size);
+	self->list = ft_memcpy(tmp + new_free_mem_mask_size + self->max_size *
+				self->item_size, self->list, old_size * sizeof(t_alst_item));
+	ft_free(self->mem);
+	self->mem = tmp;
+}
+
+size_t			alst_get_space(t_alst *self)
+{
+	size_t		res;
+	__uint64_t	tmp;
+
+	res = 0;
+	if (self->curlen == self->max_size)
+		alst_extend(self);
+	while (self->free_space_mask[res] == 0)
+		res++;
+	tmp = self->free_space_mask[res];
+	BSR_ASM(tmp);
+	self->free_space_mask[res] |= (1 << tmp);
+	return (res * 64 + tmp);
+}
+
+t_alst_item		*alst_add_head(t_alst *self, void *data)
+{
+	size_t	item_place;
+
+	item_place = alst_get_space(self);
+	ft_memmove(self->items + self->item_size * item_place, data,
+				self->item_size);
+	self->list[item_place].self = item_place;
+	if (self->curlen++ == 0)
+		self->tail = item_place;
+	self->list[item_place].next = self->head;
+	self->head = item_place;
+}
+
+t_alst_item		*alst_add_tail(t_alst *self, void *data)
+{
+	size_t	item_place;
+
+	item_place = alst_get_space(self);
+	ft_memmove(self->items + self->item_size * item_place, data,
+				self->item_size);
+	self->list[item_place].self = item_place;
+	self->list[item_place].next = ALST_SPEC_VALUE;
+	if (self->curlen++ == 0)
+	{
+		self->head = item_place;
+		self->tail = item_place;
+	}
+	else
+		self->list[self->tail].next = item_place;
+}
