fadbc824d4ef14e160916189132d34c928fde017
Midas Trisha <mtrisha@student.21-school.ru>
2020-01-14 19:57:04 +0300

    rolled away test.c to base state

--- test/test.c
+++ test/test.c
@@ -6,42 +6,14 @@
 /*   By: mtrisha <mtrisha@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 12:03:18 by mtrisha           #+#    #+#             */
-/*   Updated: 2020/01/14 19:29:44 by mtrisha          ###   ########.fr       */
+/*   Updated: 2020/01/14 19:56:18 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
-#include <math.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <float.h>
-#include <ctype.h>
 
 void	test(void)
 {
-	t_deq	deq[1];
-	deq_init(deq, sizeof(int), 32);
-
-	deq_push_front(deq, ft_i(9));
-	deq_push_front(deq, ft_i(5));
-	deq_push_front(deq, ft_i(0));
-	deq_push_back(deq, ft_i(10));
-	deq_align_front(deq);
-	deq_push_back(deq, ft_i(15));
-
-
-	deq_align_front(deq);
-
-	ft_printf("%d\n", *(int *)deq_pop_back(deq));
-	ft_printf("%d\n", *(int *)deq_pop_front(deq));
-	ft_printf("%d\n", *(int *)deq_pop_front(deq));
-	ft_printf("%d\n", *(int *)deq_pop_front(deq));
-	ft_printf("%d\n", *(int *)deq_pop_front(deq));
-
-	deq_del(deq);
 	return ;
 }
 
