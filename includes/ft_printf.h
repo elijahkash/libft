fdb5090ed80edb669015f2b71ed4def2402cf13b
Midas Trisha <mtrisha@student.21-school.ru>
2020-01-15 10:43:48 +0300

    mini fix for code beauty

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,14 +6,14 @@
 /*   By: mtrisha <mtrisha@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 16:51:11 by mtrisha           #+#    #+#             */
-/*   Updated: 2019/11/26 20:39:17 by mtrisha          ###   ########.fr       */
+/*   Updated: 2020/01/15 10:43:21 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# define HOLD_OVER_FORCE 2
+# define HOLD_OVER_FORCE 0
 # define IMMIDIATLY_FORCE 1
 
 # define FORCE_BUFF_POLICY HOLD_OVER_FORCE
