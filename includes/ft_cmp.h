591fa1a613c4c50b2399910eef24b24b1bcb50ea
Midas Trisha <mtrisha@student.21-school.ru>
2020-01-21 18:12:22 +0300

    add coment

--- includes/ft_cmp.h
+++ includes/ft_cmp.h
@@ -6,12 +6,17 @@
 /*   By: Kashnitskiy <elijahkash.code@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 15:40:51 by mtrisha           #+#    #+#             */
-/*   Updated: 2020/01/16 13:54:48 by Kashnitskiy      ###   ########.fr       */
+/*   Updated: 2020/01/21 18:10:17 by Kashnitskiy      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_CMP_H
 # define FT_CMP_H
+
+/*
+** Funcs for compare diff types.
+** Used in funcs like ft_qsort()
+*/
 
 int		ft_icmp(const void *p1, const void *p2);
 int		ft_ccmp(const void *p1, const void *p2);
