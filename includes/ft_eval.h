4ca23915c0fa276516116012596e80b77f178e23
Midas Trisha <mtrisha@student.21-school.ru>
2020-01-21 18:09:11 +0300

    add coment

--- includes/ft_eval.h
+++ includes/ft_eval.h
@@ -6,7 +6,7 @@
 /*   By: Kashnitskiy <elijahkash.code@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/04 19:57:19 by mtrisha           #+#    #+#             */
-/*   Updated: 2020/01/16 13:54:43 by Kashnitskiy      ###   ########.fr       */
+/*   Updated: 2020/01/21 18:09:05 by Kashnitskiy      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 # define FT_EVAL_H
 
 # include <string.h>
+
+/*
+** Write for t_vect, t_deq ...
+** turns value to pointer to that value!
+*/
 
 void		*ft_i(int i);
 void		*ft_z(size_t i);
