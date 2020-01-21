9225e445c62d3acaf86f0ab47ed22e1f34f24f66
Midas Trisha <mtrisha@student.21-school.ru>
2020-01-21 20:57:01 +0300

    norm test.c

--- test/test.c
+++ test/test.c
@@ -6,7 +6,7 @@
 /*   By: Kashnitskiy <elijahkash.code@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 12:03:18 by mtrisha           #+#    #+#             */
-/*   Updated: 2020/01/18 15:57:58 by Kashnitskiy      ###   ########.fr       */
+/*   Updated: 2020/01/21 20:56:52 by Kashnitskiy      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,11 @@ void	test(void)
 ** Look here if you don’t understand the meaning of such main()
 ** https://github.com/elijahkash/libft/wiki/First-steps
 */
+
 int		main(void)
 {
 	ft_memman_init();
-
 	test();
-
 	ft_force_buff();
 	ft_memman_clean();
 	return (0);
