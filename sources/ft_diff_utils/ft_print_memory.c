00d9a76d718a4791efc74e3784971f6ebb91d255
Midas Trisha <mtrisha@student.21-school.ru>
2020-01-22 21:54:47 +0300

    update print_memory (now use ft_printf)

--- sources/ft_diff_utils/ft_print_memory.c
+++ sources/ft_diff_utils/ft_print_memory.c
@@ -6,20 +6,18 @@
 /*   By: Kashnitskiy <elijahkash.code@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/21 12:53:49 by mtrisha           #+#    #+#             */
-/*   Updated: 2020/01/16 13:56:04 by Kashnitskiy      ###   ########.fr       */
+/*   Updated: 2020/01/22 21:41:10 by Kashnitskiy      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_diff_utils.h>
 
-#include <ft_libc.h>
+#include <libft.h>
 
 static void	print_hex(char c)
 {
-	if (c < 10)
-		ft_putchar('0' + c);
-	else
-		ft_putchar('a' + c - 10);
+	(c < 10) ? ft_printf("%c", ('0' + c)) :
+				ft_printf("%c", ('a' + c - 10));
 }
 
 static void	print_byte(const void *ptr)
@@ -32,42 +30,34 @@ static void	print_str(char *str, int size)
 {
 	int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (ft_isprint(str[i]))
-			ft_putchar(str[i]);
-		else
-			ft_putchar('.');
-		i++;
-	}
+	i = -1;
+	while (++i < size)
+		(ft_isprint(str[i])) ? ft_printf("%c", str[i]) :
+								ft_printf(".");
+	ft_printf("\n");
 }
 
 void		ft_print_memory(const void *addr, size_t size)
 {
 	size_t	i;
-	int		j;
 	int		sp;
 
 	sp = 40;
 	i = 0;
 	while (i < size)
 	{
-		print_byte((void *)((unsigned char *)addr + i++ + (sp -= 2) * 0));
-		if (i % 2 == 0)
-			ft_putchar(' ' + (sp-- * 0));
+		print_byte((void *)((unsigned char *)addr + i++));
+		sp -= 2;
+		(i % 2 == 0) ? ft_printf("%c", ' ' + (sp-- * 0)) : 0;
 		if (i % 16 == 0)
 		{
 			print_str((char *)addr + i - 16, 16);
-			ft_putchar('\n' + (sp = 40) * 0);
+			sp = 40;
 		}
 	}
 	if (i % 16 != 0)
 	{
-		j = 0;
-		while (j++ < sp)
-			ft_putchar(' ');
+		ft_printf("%*s", sp, "");
 		print_str((char *)addr + i - i % 16, i % 16);
-		ft_putchar('\n');
 	}
 }
