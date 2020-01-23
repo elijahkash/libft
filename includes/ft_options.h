fe041dbd15ee99ac1c00a4333955c75eba8f22fd
Midas Trisha <mtrisha@student.21-school.ru>
2020-01-23 16:48:38 +0300

    mini fix. nothing matter

--- includes/ft_options.h
+++ includes/ft_options.h
@@ -6,7 +6,7 @@
 /*   By: Kashnitskiy <elijahkash.code@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/02 13:58:10 by mtrisha           #+#    #+#             */
-/*   Updated: 2020/01/16 13:54:26 by Kashnitskiy      ###   ########.fr       */
+/*   Updated: 2020/01/23 16:48:11 by Kashnitskiy      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ typedef unsigned long long int	t_options;
 
 int								ft_is_option(const char *line);
 char							ft_check_opt(char *line, char *base);
-t_options						ft_get_options(const char *line);
 t_options						ft_get_opt_bit(const char c);
+t_options						ft_get_options(const char *line);
 
 #endif
