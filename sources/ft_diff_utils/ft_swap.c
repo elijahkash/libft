03cca87272ccf6b64d39086caf444893ed8ee488
Midas Trisha <mtrisha@student.21-school.ru>
2020-01-23 18:39:35 +0300

    update ft_swap

--- sources/ft_diff_utils/ft_swap.c
+++ sources/ft_diff_utils/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: Kashnitskiy <elijahkash.code@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/03 21:03:06 by mtrisha           #+#    #+#             */
-/*   Updated: 2020/01/16 13:56:29 by Kashnitskiy      ###   ########.fr       */
+/*   Updated: 2020/01/23 18:37:05 by Kashnitskiy      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,14 @@
 
 void			ft_swap(void *p1, void *p2, size_t size)
 {
-	char tmp[size];
+	size_t	i;
+	char	tmp;
 
-	ft_memcpy(tmp, p1, size);
-	ft_memcpy(p1, p2, size);
-	ft_memcpy(p2, tmp, size);
+	i = -1;
+	while (++i < size)
+	{
+		tmp = ((char *)p1)[i];
+		((char *)p1)[i] = ((char *)p2)[i];
+		((char *)p2)[i] = tmp;
+	}
 }
